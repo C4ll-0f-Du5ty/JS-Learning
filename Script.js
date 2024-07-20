8b4d3887af5459cd3049c7fdc101066d886e8fe0
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-20 18:32:55 +0300

    d

--- Script.js
+++ Script.js
@@ -3,9 +3,7 @@
 // alert("Let's have A look");
 // console.log(55 % 2);
 
-function calcAvg(one, two, three) {
-    return (one + two + three) / 3
-}
+const calcAvg = (one, two, three) => (one + two + three) / 3
 
 function checkWinner(avg1, avg2) {
     if (avg1 >= avg2 * 2) {
@@ -18,7 +16,8 @@ function checkWinner(avg1, avg2) {
         console.log("its A Draw")
     }
 }
-
+console.log(calcAvg(44, 23, 71), calcAvg(65, 54, 49))
 checkWinner(calcAvg(44, 23, 71), calcAvg(65, 54, 49))
+console.log(calcAvg(85, 54, 41), calcAvg(23, 34, 27))
 checkWinner(calcAvg(85, 54, 41), calcAvg(23, 34, 27))
 
