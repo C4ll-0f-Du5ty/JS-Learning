cb8f26d3bae8c8d28a967b760beb545ac5a05856
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-20 17:57:27 +0300

    d

--- Script.js
+++ Script.js
@@ -1,2 +1,22 @@
-alert("Let's have A look")
-console.log("gg")
+alert("Let's have A look");
+console.log(55 % 2);
+
+function calcAvg(one, two, three) {
+    return (one + two + three) / 3
+}
+
+function checkWinner(avg1, avg2) {
+    if (avg1 >= avg2 * 2) {
+        console.log("First team won")
+    }
+    else if (avg2 >= avg1 * 2) {
+        console.log("Second team won")
+    }
+    else {
+        console.log("its A Draw")
+    }
+}
+
+checkWinner(calcAvg(44, 23, 71), calcAvg(65, 54, 49))
+checkWinner(calcAvg(85, 54, 41), calcAvg(23, 34, 27))
+
